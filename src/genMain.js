b9f05670289b06d6d6bd458179661f49e6944eea
Erikson Kaszubowski <erikson.k@ufsc.br>
2022-11-09 16:20:59 -0300

    Create CSS file and add bumdle modules to webpack

---
+++ src/genMain.js
@@ -0,0 +1,10 @@
+const content = document.querySelector("#content");
+
+function generateHeading() {
+    const heading = document.createElement("div");
+    heading.classList.add("heading");
+    content.appendChild(heading);
+
+    headImage = document.createElement("img");
+    
+}
