23eda6e7d634cfeadafe4bf451176d8ccc68a8ef
Erikson Kaszubowski <erikson.k@ufsc.br>
2022-11-10 10:08:54 -0300

    Implement basic tab alternating function

--- src/index.js
+++ src/index.js
@@ -7,8 +7,34 @@ const content = document.querySelector("#content");
 generateHeading(content);
 
 const main = document.querySelector("main");
-
 generateAbout(main);
 
 const aboutBtn = document.getElementById("about");
+const menuBtn = document.getElementById("menu");
+const orderBtn = document.getElementById("order");
+
+aboutBtn.addEventListener("click", changeTab);
+menuBtn.addEventListener("click", changeTab);
+orderBtn.addEventListener("click", changeTab);
+
 aboutBtn.classList.add("activeTab");
+
+btns.forEach(btn => btn.addEventListener("click", changeTab));
+
+function changeTab(e) {
+    const activeBtn = document.querySelector("button.activeTab");
+    const btn = e.target;
+
+    if (activeBtn.id == btn.id) return;
+    activeBtn.classList.toggle("activeTab");
+    btn.classList.add("activeTab");
+
+    main.textContent = "";
+
+    switch (btn.id) {
+        case "about":
+            generateAbout(main);
+            break;
+
+    }
+}
